4c00cbde3770b56d226cd4128cf551d601969ace
Getis8 <bodjalev84@gmail.com>
2026-04-26 21:26:04 +0300

    fix Calculator.cpp

--- Calculator.cpp
+++ Calculator.cpp
@@ -2,15 +2,13 @@
 
 class Calculator {
 public:
-    double add(double a, double b) { return 0; }
-    double subtract(double a, double b) { return 0; }
-    double multiply(double a, double b) { return 0; }
-    double divide(double a, double b) { return 0; }
+
 };
 
 int main() {
     Calculator calc;
     std::cout << "Calculator App Started" << std::endl;
+
     return 0;
 }
 
