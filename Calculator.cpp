c5d76b9c91242691242d1d8d9adeaded19381e50
Getis8 <bodjalev84@gmail.com>
2026-04-26 21:34:36 +0300

    add "add" and "subtract" operations

--- Calculator.cpp
+++ Calculator.cpp
@@ -2,12 +2,14 @@
 
 class Calculator {
 public:
-
+double add(double a, double b) { return a + b; }
+double subtract(double a, double b) { return a - b; }
 };
 
 int main() {
     Calculator calc;
     std::cout << "Calculator App Started" << std::endl;
+    std::cout << "5 + 3 = " << calc.add(5, 3) << std::endl;
 
     return 0;
 }
