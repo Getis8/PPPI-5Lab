34c42369f53ee787f4d1f0b207d80b6e2e6ba1f8
Vova <Sirozakozak938@gmail.com>
2026-04-26 21:44:12 +0300

    Реалізовано множення

--- Calculator.cpp
+++ Calculator.cpp
@@ -4,7 +4,7 @@ class Calculator {
 public:
     double add(double a, double b) { return 0; }
     double subtract(double a, double b) { return 0; }
-    double multiply(double a, double b) { return 0; }
+    double multiply(double a, double b) { return a * b; }
     double divide(double a, double b) { return 0; }
 };
 
